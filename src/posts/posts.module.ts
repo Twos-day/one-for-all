b7767b8cfa2a639811758d9d5e17c8372edb2d55
yiminwook <grs0416@gmail.com>
2024-06-28 15:31:41 +0900

    Update: script

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,13 +1,12 @@
-import { BadRequestException, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
+import { AwsModule } from 'src/aws/aws.module';
+import { CommonModule } from 'src/common/common.module';
+import { UsersModule } from 'src/users/users.module';
 import { PostsModel } from './entities/posts.entity';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
-import { AuthModule } from 'src/auth/auth.module';
-import { UsersModule } from 'src/users/users.module';
-import { CommonModule } from 'src/common/common.module';
-
-import { AwsModule } from 'src/aws/aws.module';
 
 @Module({
   imports: [
