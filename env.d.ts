520ca7c11c4d69da1810ef0a22a53e8bedd6f1a7
yiminwook <grs0416@gmail.com>
2024-06-28 21:31:26 +0900

    Update: env type

--- env.d.ts
+++ env.d.ts
@@ -1,11 +1,11 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      readonly NODE_ENV: 'development' | 'production' | 'test';
+      readonly NODE_ENV: 'local' | 'prod' | 'test';
       readonly TZ?: string;
 
       readonly HOST: string;
-      readonly PORT: string;
+      readonly PORT: 'http' | 'https';
 
       readonly JWT_SECRET: string;
       readonly HASH_ROUNDS: string;
@@ -15,6 +15,11 @@ declare global {
       readonly DB_USERNAME: string;
       readonly DB_PASSWORD: string;
       readonly DB_DATABASE: string;
+
+      readonly AWS_RESION: string;
+      readonly AWS_S3_ACCESS_KEY: string;
+      readonly AWS_S3_SECRET_KEY: string;
+      readonly AWS_S3_BUCKET_NAME: string;
     }
   }
 }
