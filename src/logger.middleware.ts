fac0aca636055ac645d714706e2e3f7e94d7a68c
yiminwook <grs0416@gmail.com>
2024-07-10 20:25:59 +0900

    Update: logger middleware

--- src/logger.middleware.ts
+++ src/logger.middleware.ts
@@ -1,17 +1,16 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log(`Request Method: ${req.method}`);
-    console.log(`Request URL: ${req.url}`);
-    console.log(`Request Headers: ${JSON.stringify(req.headers)}`);
+    Logger.log(`Request  ${req.method} ${req.url}, HOST: ${req.headers.host}`);
+    // console.log(`Request Headers: ${JSON.stringify(req.headers)}`);
 
     // 응답이 완료된 후 실행되는 콜백 함수 등록
     res.on('finish', () => {
-      console.log(`Response Status: ${res.statusCode}`);
-      console.log(`Response Headers: ${JSON.stringify(res.getHeaders())}`);
+      // console.log(`Response Status: ${res.statusCode}`);
+      // console.log(`Response Headers: ${JSON.stringify(res.getHeaders())}`);
     });
 
     next();
