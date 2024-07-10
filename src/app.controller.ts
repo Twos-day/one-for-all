a5494040441b8c3a980245b3d9667754029a8f76
yiminwook <grs0416@gmail.com>
2024-07-10 20:21:30 +0900

    Add: LB heath check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Post()
+  @Get()
   getHello() {
+    // LB health check
     return this.appService.getHello();
   }
 }
